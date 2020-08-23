7d62a7294f5ac9f9666a1c0d92cd66a7051861f0
MeJamoLeo <reonarudo11919273@gmail.com>
2020-08-23 23:46:46 +0900

    レイアウトの作成

---
+++ backend/resources/views/hello/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.helloapp')
+@section('title', 'index')
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです.</p>
+    <p>これは<middleware>google.com</middleware>へのリンクです．</p>
+    <p>これは<middleware>yahoo.co.jp</middleware>へのリンクです．</p>
+@endsection
+
+@section('footer')
+copyright 2017 tuyano
+@endsection
