6fb80cd534776de1f89a27328df7431e7d379f21
MeJamoLeo <reonarudo11919273@gmail.com>
2020-08-28 12:35:46 +0900

    クッキーの作成した.

--- backend/app/Http/Controllers/HelloController.php
+++ backend/app/Http/Controllers/HelloController.php
@@ -4,64 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\HelloRequest;
+use Facade\FlareClient\Http\Response;
 use Validator;
 
 class HelloController extends Controller
 {
-    // クエリ文字列にバリデータを作成する．
     public function index(Request $request)
     {
-        // クエリ文字列を配列で取得し，第2引数でルールを記述する
-        $validator = Validator::make(
-            $request->query(),
-            [
-                'id' => 'required',
-                'pass' => 'required',
-            ]
-        );
-
-        if ($validator->fails()) {
-            $msg = 'クエリーに問題があります．';
+        // クッキーの内容によって処理を変える
+        if ($request->hasCookie('msg')) {
+            $msg = 'Cookie: ' . $request->cookie('msg');
         } else {
-            $msg = 'ID/PASSを受け付けました．フォームを入力ください';
-        };
-
-        return view('hello.index', ['msg' => $msg,]);
+            $msg = '※クッキーはありません';
+        }
+        return view('hello.index', ['msg' => $msg]);
     }
 
     // Validatorクラスでのエラーメッセージの表示
     public function post(Request $request)
     {
         $rules = [
-            'name' => 'required',
-            'mail' => 'email',
-            'age' => 'numeric',
+            'msg' => 'required',
         ];
-        $messages = [
-            'name.required' => '名前は必ず入力してください',
-            'mail.email' => 'メールアドレスが必要です．',
-            'age.numeric' => '年齢を整数で記入ください',
-            'age.min' => '年齢は０歳以上で記入してください．',
-            'age.max' => '年齢は200歳以下で記入してください.',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages);
 
-        // 入力値のageが数字だった場合，age, min:0を適用する
-        $validator->sometimes('age', 'min:0', function ($input) {
-            return is_numeric($input->age);
-        });
-        // 入力値のageが数字だった場合，age, max:200を適用する
-        $validator->sometimes('age', 'max:200', function ($input) {
-            return is_numeric($input->age);
-        });
+        // var_dump($this); // $thisの中身を知りたい
+        $this->validate($request, $rules);
 
-
-        if ($validator->fails()) {
-            // 入力フォームへリダイレクト
-            return redirect('/hello')
-                ->withErrors($validator) // エラーメッセージをそのまま引き継ぐ
-                ->withInput(); //送信されたフォームをそのまま引き継ぐ
-        }
-        return view('hello.index', ['msg' => '正しく入力されました！']);
+        $msg = $request->msg;
+        $body = view('hello.index', ['msg' => '「' . $msg . '」をクッキーに保存しました．']);
+        return response($body, 200)
+            ->withCookie(cookie('msg', $msg));
     }
 }
